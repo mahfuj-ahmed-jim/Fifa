7bfc19b270f157955854551dc16676cbbd86b021
mahfuj-ahmed-jim <mahfujahmedjim32@gmail.com>
2021-10-07 03:10:34 +0600

    Confirmation page ui

--- app/src/main/java/com/ai/fifa/LogInSignIn/LogInFragment.java
+++ app/src/main/java/com/ai/fifa/LogInSignIn/LogInFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.ai.fifa.R;
 
@@ -20,6 +22,15 @@ public class LogInFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    // button
+    private Button nextButton;
+
+    // sign in
+    private TextView signInButton; // using textView as a button
+    private Fragment selectedFragment = null;
+    private static String FRAGMENT_TAG;
+    private Confirmationragment confirmationragment = new Confirmationragment();
 
     public LogInFragment() {
         // Required empty public constructor
@@ -49,6 +60,28 @@ public class LogInFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_log_in, container, false);
+
+        // button
+        nextButton = view.findViewById(R.id.buttonId_next);
+
+        // textView as button
+        signInButton = view.findViewById(R.id.textViewId_signIn);
+
+        // on click listener
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //set default fragment (welcome fragment)
+                selectedFragment = confirmationragment;
+                FRAGMENT_TAG = String.valueOf(R.string.confirmation);
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.frameLayoutId, selectedFragment, FRAGMENT_TAG)
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
 
         return view;
     }
